97a1369493b9959470fb8c98bc29dd30808a5c7d
Radhakrishnan <radhakrishnan.nit@gmail.com>
2020-02-05 08:00:56 +0530

    total numbers in cart updated

--- src/Containers/Cart/CartIcon.js
+++ src/Containers/Cart/CartIcon.js
@@ -4,17 +4,22 @@ import "./CartIcon.scss";
 import { toggleCart } from "../../redux/actionCreators";
 import { connect } from "react-redux";
 const Cart = props => {
+  let totalCount = props.cartItems.reduce((a, b) => {
+    a = a + b.quantity;
+    return a;
+  }, 0);
+
   return (
     <div className="cart" onClick={props.setCartVisibility}>
       <CartIcon className="cart-icon" />
-      <span className="cart-count">{props.totalItems}</span>
+      <span className="cart-count">{totalCount}</span>
     </div>
   );
 };
 const mapStateToProps = state => {
   return {
     cartHidden: state.cart.hidden,
-    totalItems: state.cart.cartItems.length
+    cartItems: state.cart.cartItems
   };
 };
 
